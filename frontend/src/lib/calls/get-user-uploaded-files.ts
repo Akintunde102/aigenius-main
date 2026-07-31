c37b3c058ecf82d6b2386a28999b543633785b9a
Clinton <clintonphillips464@gmail.com>
2026-07-31 15:19:01 +0100

    feat: add error handling to listCodeProjects and getUserUploadedFiles functions

--- frontend/src/lib/calls/get-user-uploaded-files.ts
+++ frontend/src/lib/calls/get-user-uploaded-files.ts
@@ -2,14 +2,15 @@ import { serverCall } from '@/servercall/init';
 import { serverCalls } from '@/servercall/store';
 
 export const getUserUploadedFiles = async () => {
-
-    const response = await serverCall({
-        serverCallProps: {
-            call: serverCalls.getGatewayUploadFiles,
-
-        },
-        authorized: true,
-    });
-
-    return response;
+    try {
+        const response = await serverCall({
+            serverCallProps: {
+                call: serverCalls.getGatewayUploadFiles,
+            },
+            authorized: true,
+        });
+        return response;
+    } catch {
+        return { ok: false, data: [] };
+    }
 };
